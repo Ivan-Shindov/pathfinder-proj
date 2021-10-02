4f8855c480b0febd797686b7a9fbf8cad75d8ff4
Ivan-Shindov <shindov162@gmail.com>
2021-10-02 03:02:37 +0300

    first commit project

---
+++ src/main/java/com/softuni/pathfinderproj/PathfinderProjApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.pathfinderproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathfinderProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softuni/pathfinderproj/PathfinderProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.pathfinderproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
